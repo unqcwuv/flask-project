bebd1f8bed41ea5af72f6fa8319ee934df4837b7
Adminn <ppllaayy.155@gmail.com>
2026-01-16 16:49:03 +0700

    project structure

---
+++ app/routes.py
@@ -0,0 +1,31 @@
+from app import app
+
+@app.route('/')
+def hello():
+    return 'Hello, Flask!'
+
+# Задание 1
+@app.route('/hello')
+def hello_world():
+    return 'Hello, world!'
+
+@app.route('/info')
+def info():
+    return 'This is an informational page.'
+
+# Задание 2
+@app.route('/calc/<int:a>/<int:b>')
+def calc(a, b):
+    return f"The sum of {a} and {b} is {a + b}."
+
+# Задание 3
+@app.route('/reverse/<text>')
+def reverse(text):
+    return text[::-1]
+
+# Задание 4
+@app.route('/user/<name>/<int:age>')
+def user(name, age):
+    if age < 0:
+        return 'Error: Age cannot be negative.', 400
+    return f"Hello, {name}. You are {age} years old."

1564003e2fb32387b6d53bd88bbe0d0760111279
Adminn <ppllaayy.155@gmail.com>
2026-01-20 15:48:55 +0700

    html files

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,17 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
-def hello():
-    return 'Hello, Flask!'
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 # Задание 1
 @app.route('/hello')

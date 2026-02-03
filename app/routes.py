6cbf21cd73e1d11b2c6ef363b2371c2121c6cd2b
Adminn <ppllaayy.155@gmail.com>
2026-02-03 14:58:27 +0700

    funny form

--- app/routes.py
+++ app/routes.py
@@ -1,40 +1,18 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from app import app
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def form():
+    return render_template('form.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-# Задание 1
-@app.route('/hello')
-def hello_world():
-    return 'Hello, world!'
-
-@app.route('/info')
-def info():
-    return 'This is an informational page.'
-
-# Задание 2
-@app.route('/calc/<int:a>/<int:b>')
-def calc(a, b):
-    return f"The sum of {a} and {b} is {a + b}."
-
-# Задание 3
-@app.route('/reverse/<text>')
-def reverse(text):
-    return text[::-1]
-
-# Задание 4
-@app.route('/user/<name>/<int:age>')
-def user(name, age):
-    if age < 0:
-        return 'Error: Age cannot be negative.', 400
-    return f"Hello, {name}. You are {age} years old."
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        color = request.form.get('color')
+        profession = request.form.get('profession')
+        hobbies = request.form.getlist('hobbies')
+        level = request.form.get('level')
+        return render_template('result.html', name=name, color=color, profession=profession, hobbies=hobbies, level=level)
+    else:
+        return redirect(url_for('form'))
